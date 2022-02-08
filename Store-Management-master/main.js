fe55338a03261f9a36ad9f5b87897ae64b5efb82
abdelbasset4 <benatmani1999@gmail.com>
2022-02-08 22:32:38 +0100

    V2 store managment app

--- Store-Management-master/main.js
+++ Store-Management-master/main.js
@@ -14,27 +14,28 @@ document.querySelector("#create-product").addEventListener("click", () => {
     taxes: document.querySelector("#product-taxes").value,
     ads: document.querySelector("#product-adg").value,
     discount: document.querySelector("#product-discount").value,
-    total: document.querySelector(".result").value,
+    total: document.querySelector(".result").innerHTML,
     category: document.querySelector("#product-category").value,
   };
-  testFields();
-  let tbody = document.querySelector("tbody");
-  for (let i = 0; i < count; i++) {
-    product.id = products.length;
-    tbody.append(
-      productTableRow(
-        product.id,
-        product.name,
-        product.price,
-        product.taxes,
-        product.ads,
-        product.discount,
-        product.total,
-        product.category
-      )
-    );
-    products.push(product);
-  }
+  testFields(product);
+  // let tbody = document.querySelector("tbody");
+
+  // for (let i = 0; i < count; i++) {
+  //   product.id = products.length;
+  //   tbody.append(
+  //     productTableRow(
+  //       product.id,
+  //       product.name,
+  //       product.price,
+  //       product.taxes,
+  //       product.ads,
+  //       product.discount,
+  //       product.total,
+  //       product.category
+  //     )
+  //   );
+  //   products.push(product);
+  // }
 });
 function productTableRow(
   id,
@@ -60,7 +61,7 @@ function productTableRow(
   return row;
 }
 
-function testFields() {
+function testFields(product) {
   let fields = [
     document.querySelector("#product-name"),
     ...Array.from(
@@ -70,11 +71,75 @@ function testFields() {
     ),
     document.querySelector("#product-count"),
   ];
-  fields.forEach((ele) => {
-    ele.style.border = "none";
-    if (ele.value === "") {
-      ele.style.border = "1px solid red";
+  let isEmpty = fields.some((ele) => ele.value === "");
+  let isNegative = fields.slice(1).some((ele) => ele.value <0 );
+  if (isEmpty) {
+    alert("fill those fields");
+    fields.forEach((ele) => {
+      ele.style.border = "none";
+      if (ele.value === "") {
+        ele.style.border = "1px solid red";
+      }
+    });
+  } else if (isNegative) {
+    alert("some value is negative");
+    fields.forEach((ele) => {
+      ele.style.border = "none";
+      if (ele.value <0) {
+        ele.style.border = "1px solid red";
+      }
+    });
+  } else {
+    let tbody = document.querySelector("tbody");
+    let count = document.querySelector("#product-count").value;
+    for (let i = 0; i < count; i++) {
+      product.id = products.length;
+      tbody.append(
+        productTableRow(
+          product.id,
+          product.name,
+          product.price,
+          product.taxes,
+          product.ads,
+          product.discount,
+          product.total,
+          product.category
+        )
+      );
+      products.push(product);
     }
-  });
-  alert("fill those fields");
+  }
 }
+
+document.querySelectorAll(".product-price-controllers input").forEach((ele) => {
+  ele.addEventListener("change", (e) => {
+    let result = document.querySelector(".result");
+    let checkFields = Array.from(
+      document.querySelectorAll(
+        ".product-price-controllers input:not(:last-of-type)"
+      )
+    );
+    let isEmpty = checkFields.some((ele) => ele.value === "");
+    if (isEmpty) {
+      result.textContent = "";
+    } else {
+      console.log(checkFields);
+
+      let total = checkFields
+        .map((ele) => Number(ele.value))
+        .reduce(function (previousValue, currentValue) {
+          return previousValue + currentValue;
+        }, 0);
+      result.textContent = total;
+    }
+
+    result.textContent =
+      result.textContent - document.querySelector("#product-discount").value;
+  });
+});
+
+
+let addCategory = document.getElementById("add-category");
+addCategory.addEventListener("click", () => {
+  document.querySelector(".popup-container").style.display = "block";
+})
